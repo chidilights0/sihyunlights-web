8abe3e065640d694606911394306d9ea23cf2ccb
sihyunlights <62822977+sihyunlts@users.noreply.github.com>
2024-11-04 18:32:08 +0900

    Build fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { HashRouter as MemoryRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Main from './pages/Main';
-import Projects from './pages/Projects';
-import History from './pages/History';
+import Main from "./pages/Main";
+import Projects from "./pages/Projects";
+import History from "./pages/History";
 
-import './App.css';
-import Transition from './components/Transition';
-import { AnimatePresence } from 'framer-motion';
-import { HeaderProvider } from './components/HeaderContext';
+import "./App.css";
+import Transition from "./components/Transition";
+import { AnimatePresence } from "framer-motion";
+import { HeaderProvider } from "./components/HeaderContext";
 
 function App() {
   return (
-      <Router>
-        <HeaderProvider>
-        <Header/>
+    <MemoryRouter>
+      <HeaderProvider>
+        <Header />
         <AnimatePresence>
-        <Routes>
-          <Route path='/' element={<Transition><Main/></Transition>}/>
-          <Route path='projects' element={<Transition><Projects/></Transition>}/>
-          <Route path='history' element={<Transition><History/></Transition>}/>
-        </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Transition>
+                  <Main />
+                </Transition>
+              }
+            />
+            <Route
+              path="/projects"
+              element={
+                <Transition>
+                  <Projects />
+                </Transition>
+              }
+            />
+            <Route
+              path="/history"
+              element={
+                <Transition>
+                  <History />
+                </Transition>
+              }
+            />
+          </Routes>
         </AnimatePresence>
-        </HeaderProvider>
-        <Footer/>
-      </Router>
+      </HeaderProvider>
+      <Footer />
+    </MemoryRouter>
   );
 }
 
